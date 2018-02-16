7102cc0372790aea8949256bf124ea152dbd54cf
sonutiwari <ankorha@gmail.com>
2018-02-16 07:49:50 +0530

    Accomodating all changes suggested by reviewer

--- app/src/main/java/com/gadgetsmend/cricketscorecounter/MainActivity.java
+++ app/src/main/java/com/gadgetsmend/cricketscorecounter/MainActivity.java
@@ -2,69 +2,57 @@ package com.gadgetsmend.cricketscorecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    TextView scoreViewTeamA;
+    TextView scoreViewTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //displayForTeamA(8);
-    }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        return true;
+        scoreViewTeamA = findViewById(R.id.displayScoreTeamA);
+        scoreViewTeamB = findViewById(R.id.displayScoreTeamB);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = getItemId();
-        if(id == R.id.action_settings){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-*/
-
-
-    /*
-     *Add 1 in the Score of Team A
-    */
+    /**
+     * Add 1 in the Score of Team A
+     * @param v
+     */
     public void addOneForTeamA(View v){
         scoreTeamA++;
         displayForTeamA(scoreTeamA);
 
     }
 
-    /*
-     *Add 2 in the Score of Team A
-    */
+    /**
+     * Add 2 in the Score of Team A
+     * @param v
+     */
     public void addTwoForTeamA(View v){
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
 
     }
 
-    /*
-     *Add 3 in the Score of Team A
-    */
+    /**
+     * Add 3 in the Score of Team A
+     * @param v
+     */
     public void addThreeForTeamA(View v){
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
 
     }
 
-    /*
-     *Add 4 in the Score of Team A
-    */
+    /**
+     * Add 4 in the Score of Team A
+     * @param v
+     */
     public void addFourForTeamA(View v){
         scoreTeamA = scoreTeamA + 4;
         displayForTeamA(scoreTeamA);
@@ -72,9 +60,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /*
-     *Add 6 in the Score of Team A
-    */
+    /**
+     * Add 6 in the Score of Team A
+     * @param v
+     */
     public void addSixForTeamA(View v){
         scoreTeamA += 6;
         displayForTeamA(scoreTeamA);
@@ -83,32 +72,35 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Displays the given score for Team A.
+     * @param score
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.displayScoreTeamA);
-        scoreView.setText(String.valueOf(score));
+        scoreViewTeamA.setText(String.valueOf(score));
     }
 
-    /*
-     *Add 1 in the Score of Team B
-    */
+    /**
+     * Add 1 in the Score of Team B
+     * @param v
+     */
     public void addOneForTeamB(View v){
         scoreTeamB++;
         displayForTeamB(scoreTeamB);
 
     }
 
-    /*
-     *Add 2 in the Score of Team B
-    */
+    /**
+     * Add 2 in the Score of Team B
+     * @param v
+     */
     public void addTwoForTeamB(View v){
         scoreTeamB += 2;
         displayForTeamB(scoreTeamB);
 
     }
 
-    /*
-     *Add 3 in the Score of Team B
+    /**
+     * Add 3 in the Score of Team B
+     * @param v
     */
     public void addThreeForTeamB(View v){
         scoreTeamB += 3;
@@ -116,9 +108,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*
-     *Add 4 in the Score of Team B
-    */
+    /**
+     * Add 4 in the Score of Team B
+     * @param v
+     */
     public void addFourForTeamB(View v){
         scoreTeamB += 4;
         displayForTeamB(scoreTeamB);
@@ -126,25 +119,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /*
-     *Add 6 in the Score of Team B
+    /**
+     * Add 6 in the Score of Team B
+     * @param v
     */
     public void addSixForTeamB(View v){
         scoreTeamB += 6;
         displayForTeamB(scoreTeamB);
-
     }
 
     /**
-     * Displays the given score for Team A.
+     * Displays the given score for Team B.
+     * @param score
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.displayScoreTeamB);
-        scoreView.setText(String.valueOf(score));
+        scoreViewTeamB.setText(String.valueOf(score));
     }
 
-    /* Reset Scores of both
-     * A and B
+    /** Reset Scores of both A and B
+     * @param v
      */
 
     public  void  resetScore(View v){
